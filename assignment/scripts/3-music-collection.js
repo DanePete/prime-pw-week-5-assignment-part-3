f57437eab37d11cfebd259a1df7cc0cc2d8172f3
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-11 13:46:34 -0500

    created function to add an object to an array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function AddToCollection(title, artist, yearPublished) {
+  const album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+
+  collection.push(album);
+
+}
+
+AddToCollection('The Mollusk', 'Ween', '1997');
+console.log(collection);
