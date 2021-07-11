86ffcc1051afef7db84ca77c549279f9885dcd91
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-11 13:51:01 -0500

    called add to collection function 7 times

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,4 +14,11 @@ function AddToCollection(title, artist, yearPublished) {
 }
 
 AddToCollection('The Mollusk', 'Ween', '1997');
+AddToCollection('Pure Guava', 'Ween', '1992');
+AddToCollection('American Beauty', 'Grateful Dead', '1970');
+AddToCollection('Rift', 'Phish', '1993');
+AddToCollection('Animals', 'Pink Floyd', '1977');
+AddToCollection('All Things Must Pass', 'George Harrison', '1970');
+AddToCollection('Trans-Continental Hustle', 'Gogol Bordello', '2010');
+
 console.log(collection);
