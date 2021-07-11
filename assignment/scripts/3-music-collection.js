065530e2f1001fbec71cac5e102f0c3f5b479746
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-11 13:57:46 -0500

    created show collection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,10 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+/*
+* Add to Collection
+* Adds objects into an array
+*/
 function AddToCollection(title, artist, yearPublished) {
   const album = {
     title: title,
@@ -10,7 +14,6 @@ function AddToCollection(title, artist, yearPublished) {
   };
 
   collection.push(album);
-
 }
 
 AddToCollection('The Mollusk', 'Ween', '1997');
@@ -22,3 +25,24 @@ AddToCollection('All Things Must Pass', 'George Harrison', '1970');
 AddToCollection('Trans-Continental Hustle', 'Gogol Bordello', '2010');
 
 console.log(collection);
+
+/*
+* Show Collection
+* Requests a specific object
+*/
+
+function showCollection(array) {
+  console.log(array.length);
+  for (const item of array){
+    console.log(item.title, 'by', item.artist, 'published in', item.yearPublished);
+  } 
+}
+
+showCollection(collection);
+
+
+
+
+// Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+// Console.log the number of items in the array.
+// Loop over the array and console.log each album's information formatted like: TITLE by ARTIST, published in YEAR.
